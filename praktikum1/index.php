170e9da9095270adfb08346ec12f07b4cb144551
fathifarhat <fathifarhatpsp7@gmail.com>
2023-02-24 16:08:21 +0700

    upload praktikum 1

---
+++ praktikum1/index.php
@@ -0,0 +1,35 @@
+<?php 
+// ini sebuah komentar
+/* ini sebuah komentar */
+# komentar pake simbol kres
+
+
+echo "Hello World <br>";
+echo "Hello World <br>";
+print_r("Fathi Farhat <br>");
+echo "<hr>";
+
+
+$nama = "Fathi Farhat";
+$umur = 18;
+$berat = 65.2;
+$ahasiswa = true;
+
+echo "Nama saya $nama <br>";
+echo "Umur saya sekarang adalah $umur tahun <br>";
+echo "Berat saya $berat kg <br>";
+echo "<hr>"
+
+
+// Membuat array
+$programs = ["php", "javascrip", "html", "css"];
+echo $programs[0];
+foreach ($programs as $program){
+    echo $programs;
+}
+
+
+
+
+
+?>
