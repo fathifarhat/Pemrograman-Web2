32734bd373a1fe78a5180a4dc342193178eee2e2
fathifarhat <fathifarhatpsp7@gmail.com>
2023-03-10 15:56:43 +0700

    upload praktikum 3

---
+++ praktikum3/proses_registrasi.php
@@ -0,0 +1,20 @@
+<?php 
+$domisili = ["Jakarta", "Bogor", "Depok", "Tangerang", "Bandung"];
+
+$program_studi = [
+    "SI"=>"Sistem Informasi",
+    "TI"=>"Teknik Informatika",
+    "BD"=>"Bisnis Digital",
+];
+
+$skill_user = [
+    "HTML"=>10,
+    "CSS"=>10,
+    "Javascript"=>20,
+    "RWD Bootstrap"=>20,
+    "PHP"=>30,
+    "Python"=>30,
+    "Java"=>50
+];
+
+?>
